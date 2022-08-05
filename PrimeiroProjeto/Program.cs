f8dc7dd4b4939dfd7a160651a32bbc23f59efaee
aluno <Aluno@DOSO026570.educacional.up>
2022-08-04 21:06:26 -0300

    Atualizando

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odeio que todo projeto começe com um Hello World!");
+        }
+    }
+}
