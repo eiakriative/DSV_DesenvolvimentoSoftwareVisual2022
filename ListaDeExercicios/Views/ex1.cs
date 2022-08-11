168c8da0f665ddde04589a7b2fc46782cfe82e98
alhkanashiro <alhkanashiro@gmail.com>
2022-08-11 20:46:13 -0300

    Atualizado!!!

--- ListaDeExercicios/Views/ex1.cs
+++ ListaDeExercicios/Views/ex1.cs
@@ -6,20 +6,18 @@ namespace ListaDeExercicios.Views
     {
         public static void Renderizar()
         {
-            int area;
             Console.WriteLine("Digite a largura:");
             int largura = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Digite a altura:");
             int altura = Convert.ToInt32(Console.ReadLine());
-            area = altura * largura;
+            int area = altura * largura;
             Console.WriteLine($"Área: {area} ");
 
-            // double area;
             // Console.WriteLine("Digite a largura:");
             // double largura = Convert.ToDouble(Console.ReadLine());
             // Console.WriteLine("Digite a altura:");
             // double altura = Convert.ToDouble(Console.ReadLine());
-            // area = altura * largura;
+            // double area = altura * largura;
             // Console.WriteLine($"Área: {area} ");
         }
     }
