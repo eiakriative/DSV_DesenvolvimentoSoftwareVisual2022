b9be2d1e4c7507942082f647ce16870c9654998a
alhkanashiro <alhkanashiro@gmail.com>
2022-08-18 20:03:03 -0300

    aula 3

--- Aula 3/OrientacaoObjetos/Produto.cs
+++ Aula 3/OrientacaoObjetos/Produto.cs
@@ -2,6 +2,8 @@ namespace OrientacaoObjetos
 {
     class Produto
     {
+        public Produto() {}
+        
         public string Nome { get; set;} 
         public double Preco { get; set;}
         
--- Aula 3/OrientacaoObjetos/Program.cs
+++ Aula 3/OrientacaoObjetos/Program.cs
@@ -10,7 +10,18 @@ namespace OrientacaoObjetos
             usr.setNome("Joel");
             Console.WriteLine($"Nome do usuario { usr.getNome() }");
             Console.WriteLine("----------------------------------");
-
+            Produto prod = new Produto();
+            prod.Nome = "Tenis Nike";
+            prod.Preco = 200.50;
+             Console.WriteLine($"Nome: { prod.Nome } \nPreço: { prod.Preco}");
+            Console.WriteLine("----------------------------------");
+            Produto prod2 = new Produto
+            {
+                Nome = "Pepsi",
+                Preco = 5
+            };
+             Console.WriteLine($"Nome: { prod2.Nome } \nPreço: { prod2.Preco}");
+             Console.WriteLine("----------------------------------");
         }
     }
 }
