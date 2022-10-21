c6eb516008f6d190e60058b6fab1999cd2aed2d2
alhkanashiro <alhkanashiro@gmail.com>
2022-10-20 22:13:54 -0300

    Add formulario

--- Aula 3/FRONT_Folhas/src/app/app-routing.module.ts
+++ Aula 3/FRONT_Folhas/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { ListarFuncionariosComponent } from './components/pages/funcionario/listar-funcionarios/listar-funcionarios.component';
+import { CadastrarFuncionarioComponent } from './components/pages/funcionario/cadastrar-funcionario/cadastrar-funcionario.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "pages/funcionario/cadastrar",
+    component: CadastrarFuncionarioComponent
+  },
+  {
+    path: "pages/funcionario/listar",
+    component: ListarFuncionariosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
